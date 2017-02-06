4d81c71b01031e50dc5aa86a4f2cc737723103e0
Kamila Tomczyk <ka1130@o2.pl>
2017-02-06 15:22:01 +0100

    Add GameOfLife contsructor.

--- js/app.js
+++ js/app.js
@@ -2,5 +2,13 @@
     document.addEventListener("DOMContentLoaded", function(event) {
         console.log("ok");
 
+        function GameOfLife(boardWidth, boardHeight) {
+            this.width = boardWidth;
+            this.height = boardHeight;
+        }
+
+        const game = new GameOfLife(10, 10);
+        console.log(game);
+
     });
 })();

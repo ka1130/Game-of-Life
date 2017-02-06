c24d3275e554faca70c371ac3c1fe03d9b2a330c
Kamila Tomczyk <ka1130@o2.pl>
2017-02-06 18:41:37 +0100

    Computing next state.

--- js/app.js
+++ js/app.js
@@ -48,10 +48,29 @@
             this.setCellState(4, 0, "live");
         }
 
+        GameOfLife.prototype.computeCellNextState = function(x, y) {
+            let neighbours = [this.getCellIndex(x - 1, y - 1), this.getCellIndex(x, y - 1), this.getCellIndex(x + 1, y - 1), this.getCellIndex(x - 1, y), this.getCellIndex(x + 1, y), this.getCellIndex(x - 1, y + 1), this.getCellIndex(x, y + 1), this.getCellIndex(x + 1, y + 1)];
+            console.log(neighbours);
+            console.dir(this.getCellIndex(x, y));
+            neighbours.forEach(function(item, index) {
+                console.log(item.classList == true);
+                return (item.classList !== "");
+            });
+        }
+
+        GameOfLife.prototype.computeNextGeneration = function() {
+
+        }
+
+        GameOfLife.prototype.printNextGeneration = function() {
+
+        }
+
         game.createBoard();
         // game.getCellIndex(5, 1);
         // game.setCellState(3, 4, "live");
         game.firstGlider();
+        game.computeCellNextState(5, 1);
 
 
 

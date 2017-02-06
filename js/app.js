fb14be25dea1daafd55a7ebc13d30e8419be97de
Kamila Tomczyk <ka1130@o2.pl>
2017-02-06 15:32:59 +0100

    Add board property to the contsructor, add createBoard() method.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,20 @@
 (function() {
     document.addEventListener("DOMContentLoaded", function(event) {
-        console.log("ok");
 
         function GameOfLife(boardWidth, boardHeight) {
             this.width = boardWidth;
             this.height = boardHeight;
+            this.board = document.getElementById("board");
+
         }
 
         const game = new GameOfLife(10, 10);
         console.log(game);
+
+        GameOfLife.prototype.createBoard() = function() {
+
+        };
+
 
     });
 })();
